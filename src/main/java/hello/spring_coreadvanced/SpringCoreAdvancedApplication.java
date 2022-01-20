5f1e7ded0add182a4a82781987c32c233c27dda2
johanpark <pyhan23@gmail.com>
2022-01-20 23:35:57 +0900

    Initial commit

---
+++ src/main/java/hello/spring_coreadvanced/SpringCoreAdvancedApplication.java
@@ -0,0 +1,13 @@
+package hello.spring_coreadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoreAdvancedApplication.class, args);
+    }
+
+}
